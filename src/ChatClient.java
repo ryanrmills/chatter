2d2e694f006e4bd060b2ebf87eab50246fc7c29b
ryanrmills <ryanroymills@gmail.com>
2025-11-30 21:15:12 -0800

    chatclient

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,9 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.io.FileInputStream;
 
 // To recompile files and run client:
 // javac src/*.java && java -cp src ChatClient
@@ -6,14 +11,37 @@ import java.io.IOException;
 public class ChatClient {
 
     public static void main(String[] args) {
-       
+        try {
+            readFromSocket("localhost", 12345);
+        } catch (IOException e) {
+            System.err.println("Error reading from socket");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 
-    public static void readFromFile(String filename) throws IOException{
+    public static void readFromFile(String filename) throws IOException {
+        InputStream inputStream = new FileInputStream(filename);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                java.nio.charset.StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        
+        String line;
+        while ((line = bufferedReader.readLine()) != null){
+            System.out.println(line);
+        }
 
     }
 
     public static void readFromSocket(String host, int port) throws IOException {
-        
+        Socket socket = new Socket(host, port);
+        InputStream inputStream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null){
+            System.out.println(line);
+        }
     }
 }
